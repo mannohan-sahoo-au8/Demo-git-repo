e00334f32366bf0879a2cbfd25cc2840c018b2a9
Manmohan sahoo <manmohansahoo361@gmail.com>
2020-07-07 20:53:22 +0530

    Day-04

---
+++ day-04/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Form from "./components/Form";
+import Button from "./components/Button";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Login Form</h1>
+      <Form />
+      <Button />
+    </div>
+  );
+}
+
+export default App;
