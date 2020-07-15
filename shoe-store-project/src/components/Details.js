3d5bfb9112d7af5cc6db1987b3f8eb106a3e3566
Manmohan sahoo <manmohansahoo361@gmail.com>
2020-07-15 22:47:42 +0530

    Shoe-store-app

---
+++ shoe-store-project/src/components/Details.js
@@ -0,0 +1,54 @@
+import React, { Component } from "react";
+import { ProductConsumer } from "../context";
+import { Link } from "react-router-dom";
+import { ButtonContainer } from "./Button";
+
+export default class Details extends Component {
+  render() {
+    return (
+      <ProductConsumer>
+        {(value) => {
+          const { id, img, price, title, inCart } = value.detailProduct;
+          return (
+            <div className="container py-5">
+              <div className="row">
+                <div className="col-10 mx-auto text-center text-slanted text-blue my-5">
+                  <h1>{title}</h1>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                  <img src={img} className="img-fluid" alt="product" />
+                </div>
+                <div className="col-10 mx-auto col-md-6 my-3 text-capitalize">
+                  <h2>model:{title}</h2>
+                  <h4 className="text-blue">
+                    <storng>
+                      price:: <span>$</span> {price}
+                    </storng>
+                  </h4>
+                  <div>
+                    <Link to="/">
+                      <ButtonContainer> Back To Products </ButtonContainer>
+                    </Link>
+                    <ButtonContainer
+                      cart
+                      disabled={inCart ? true : false}
+                      onClick={() => {
+                        value.addToCart(id);
+                        value.openModal(id);
+                      }}
+                    >
+                      {" "}
+                      {inCart ? "inCart" : "Add To Cart"}{" "}
+                    </ButtonContainer>
+                  </div>
+                </div>
+              </div>
+            </div>
+          );
+        }}
+      </ProductConsumer>
+    );
+  }
+}
---
+++ shoe-store-project/src/data.js
@@ -0,0 +1,88 @@
+export const storeProducts = [
+  {
+    id: 1,
+    title: "Nike - Black",
+    img: "img/product-1.jpg",
+    price: 10,
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 2,
+    title: "Adidas",
+    img: "img/product-2.jpg",
+    price: 16,
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 3,
+    title: "Nike",
+    img: "img/product-3.jpg",
+    price: 8,
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 4,
+    title: "Addidas",
+    img: "img/product-4.jpg",
+    price: 18,
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 5,
+    title: "Wood Land",
+    img: "img/product-5.jpg",
+    price: 24,
+
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 6,
+    title: "Fila",
+    img: "img/product-6.jpg",
+    price: 17,
+
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 7,
+    title: "Reebok",
+    img: "img/product-7.jpg",
+    price: 30,
+
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+  {
+    id: 8,
+    title: "Lee-Cooper",
+    img: "img/product-8.jpg",
+    price: 2,
+
+    inCart: false,
+    count: 0,
+    total: 0,
+  },
+];
+
+export const detailProduct = {
+  id: 1,
+  title: "Nike - Black",
+  img: "img/product-1.jpg",
+  price: 10,
+  inCart: false,
+  count: 0,
+  total: 0,
+};
