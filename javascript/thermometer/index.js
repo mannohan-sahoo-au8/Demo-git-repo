050587350d55e4549f1bc297eae24753d8c56552
Manmohan Sahoo <manmohansahoo361@gmail.com>
2021-04-10 17:39:59 +0530

    thermometer

--- javascript/thermometer/index.js
+++ javascript/thermometer/index.js
@@ -6,7 +6,25 @@ const tempLoad = () => {
   let temp =document.querySelector("#fa")
   temp.innerHTML = "&#xf2cb"
 
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2ca"
+  },1000)
+
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c9"
+  },2000)
+
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c8"
+  },3000)
+
+  setTimeout(() => {
+    temp.innerHTML = "&#xf2c7"
+  },4000)
 }
 
 
 tempLoad() 
+setInterval(() => {
+  tempLoad()
+}, 5000);
