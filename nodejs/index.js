eaf7fd0c3e29f7d00fc5d8fff0d94bf296067cba
manmohan-sahoo-au8 <manmohansahoo361@gmail.com>
2022-08-07 15:11:59 +0530

    A

--- nodejs/index.js
+++ nodejs/index.js
@@ -1,1 +1,16 @@
 const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const logger = require('morgan');
+const { urlencoded } = require('body-parser');
+
+app.use(logger('dev'));
+app.use(bodyParser.urlencoded({ extends: false }));
+
+app.get('/', (req, res) => {
+  res.json({ tuto: 'building api with nodejs' });
+});
+
+app.listen(3000, () => {
+  console.log(`server is runngin on port {3000}`);
+});
